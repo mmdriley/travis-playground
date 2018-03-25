14916960686386213002523ea744daf9add7402e
Matthew Riley <mattdr@pulumi.com>
2018-03-25 00:45:45 -0700

    Implement cancellation

--- travisserializer/main.go
+++ travisserializer/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"net/url"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/parnurzeal/gorequest"
 )
@@ -50,6 +53,8 @@ type Builds struct {
 	Builds []Build
 }
 
+// running push builds, in this repository, on this branch
+// sorted by ID descending
 func runningBuilds() []Build {
 	var builds Builds
 
@@ -67,11 +72,60 @@ func runningBuilds() []Build {
 		Set("Authorization", "token "+travisToken).
 		EndStruct(&builds)
 
-	if errs != nil || resp.StatusCode != 200 {
+	if errs != nil || resp.StatusCode != http.StatusOK {
 		panic("can't list running builds")
 	}
 
 	return builds.Builds
+}
+
+func isOldestRunningBuild() bool {
+	builds := runningBuilds()
+	foundThisBuild := false
+
+	// The list of builds is ordered from newest to oldest.
+	for _, build := range builds {
+		if foundThisBuild {
+			// There was a running build older than this one.
+			fmt.Printf("Found older build %v (%v) in state %v\n", build.Number, build.ID, build.State)
+			return false
+		}
+
+		if strconv.Itoa(build.ID) == travisBuildID {
+			foundThisBuild = true
+		}
+	}
+
+	// If we found this build, it was last in the list.
+
+	if !foundThisBuild {
+		// Sanity check -- we should always see the current build in the list.
+		panic(fmt.Errorf("couldn't find this build, %v", travisBuildID))
+	}
+
+	return true
+}
+
+func cancelThisBuild() {
+	fmt.Printf("Cancelling this build...\n")
+
+	path := mustParseURL(fmt.Sprintf("/build/%v/cancel", travisBuildID))
+
+	resp, _, errs := gorequest.New().
+		Post(travisEndpoint.ResolveReference(path).String()).
+		Set("Travis-API-Version", "3").
+		Set("Authorization", "token "+travisToken).
+		End()
+
+	if errs != nil || resp.StatusCode != http.StatusOK {
+		panic("couldn't cancel build")
+	}
+
+	// Wait for the build to be cancelled. Travis' build timeout is 2 hours.
+	time.Sleep(3 * time.Hour)
+}
+
+func restartNewestCancelledBuild() {
 }
 
 func main() {
@@ -83,9 +137,11 @@ func main() {
 	command := os.Args[1]
 
 	if command == "start" {
-		fmt.Printf("%+v\n", runningBuilds())
+		if !isOldestRunningBuild() {
+			cancelThisBuild()
+		}
 	} else if command == "finish" {
-
+		restartNewestCancelledBuild()
 	} else {
 		fmt.Fprintf(os.Stderr, "ERROR: Invalid command %v\n", command)
 		os.Exit(1)
